42fc89e606ddffafd10a041cfd08505ddf43e5a3
Szymon Maksymowicz <131286526+maxio02@users.noreply.github.com>
2024-10-22 20:20:50 +0200

    feat: load-in animation working (#1)
    * feat: load-in animation working
    
    * feat: animation rotating with logo
    
    * fix: firefox compatibility
    
    * fix: retina display compatibility
    
    ---------
    
    Co-authored-by: Maurycy Jakiel <62938740+Jobieskii@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App({ center }) {
   function handleFileChange(e) {
     if (e.target.files && e.target.files[0]) {
       setObjectUrl(URL.createObjectURL(e.target.files[0]))
-      setFile(e.target.files[0]);  
+      setFile(e.target.files[0]);
     } else {
       setObjectUrl(null);
       setFile(null);
@@ -90,13 +90,13 @@ function App({ center }) {
           if (map && tileInView(map, updated)) {
             invalidateMapCache(map)
           };
-      });
+        });
       },
       onWebSocketError: (e) => console.log(e),
       onStompError: (e) => console.log(e)
     });
     stompClient.activate();
-    
+
     fetch("https://bib.bohenek.xyz/api/session")
       .then(e => {
         if (e.ok) {
@@ -106,16 +106,20 @@ function App({ center }) {
         }
       })
       .catch(e => e);
+    // magick, this fixes all retina display issues for the animation
+    const scaling = Browser.retina ? 0.5 : 1;
+    document.documentElement.style.setProperty('--scaling', scaling);
+
     return () => stompClient.deactivate();
   }, [map])
 
   return <div className="App">
     { map ? <Overlay 
       map={map}
-      imageObjectUrl={objectUrl} 
+      imageObjectUrl={objectUrl}
       scale={scale}
-      imgSize={imageSize} 
-      onSetScale={setScale} 
+      imgSize={imageSize}
+      onSetScale={setScale}
       onSetOffset={setFinalOffset}
       onSetImgSize={setImageSize}
     ></Overlay> : null }
@@ -164,6 +168,120 @@ function Logmap({ center }) {
       console.log(mapEvents.getZoom(), getZoomMultiplier(mapEvents.getZoom()), Browser.retina )
     }
   })
+
+  useEffect(() => {
+    mapEvents.eachLayer((layer) => {
+
+      const onTileLoad = function (event) {
+        const tile = event.tile;
+
+        if (tile.parentNode && tile.parentNode.classList.contains("bib-tile-wrapper")) {
+          const wrapper = tile.parentNode;
+          // after a random interval add the aninmation
+          setTimeout(() => {
+            wrapper.classList.add("bib-tile-wrapper--animated");
+          }, Math.random() * 1500 + 100);
+        }
+      };
+
+
+      const onTileLoadStart = function (event) {
+        const tile = event.tile;
+        // remove the transform from leaflet and add class to flip the tiles
+
+        if (tile.hasAttribute("tile-wrapped")) {
+          return;
+        }
+        tile.setAttribute("tile-wrapped", "true");
+
+        tile.style.transform = "";
+        tile.classList.add("bib-leaflet-tile");
+
+        const wrapper = document.createElement("div");
+        wrapper.classList.add("bib-tile-wrapper");
+
+        tile.parentNode.insertBefore(wrapper, tile);
+        wrapper.appendChild(tile);
+
+        const flippedTile = document.createElement("img");
+        flippedTile.classList = tile.classList;
+        flippedTile.classList.add("bib-leaflet-tile--flipped");
+
+        // change this to get the infinity symbol
+
+        // flippedTile.src = 'android-chrome-512x512.png';
+        flippedTile.src = 'loading-bib-tile.png';
+        wrapper.appendChild(flippedTile);
+
+        // put back the translate as a indivual property instead of using transform
+        wrapper.style.translate = `${tile._leaflet_pos.x}px ${tile._leaflet_pos.y}px`;
+
+        // on the 404 trigger the flip animation 
+        tile.addEventListener('error', (event) => {
+          setTimeout(() => {
+            wrapper.classList.add("bib-tile-wrapper--animated");
+
+          }, Math.random() * 400 + 100);
+        });
+
+        // this brings everything back to the state that leaflet wants it in
+        wrapper.addEventListener('transitionend', (event) => {
+          tile.style.translate = wrapper.style.translate;
+
+          const parent = wrapper.parentNode;
+          if (parent) {
+            tile.classList.remove('bib-leaflet-tile');
+            tile.removeAttribute('tile-wrapped')
+            parent.insertBefore(tile, wrapper);
+            parent.removeChild(wrapper);
+          }
+        });
+      };
+
+
+      // this has to be done, because leaflet deletes the img from the wrapper on unload or abort, can happen on slow internet or on scrolling while its animating
+      // leaflet also deletes the tile/img first and then gives the event so, I cannot access the parent wrapper, so I basically have to search for dangling wrappers
+      // this function fucking sucks, its slow, and on every unload will do a shit ton of checks, but I give up
+
+      // the solution would be to just keep a map of all the tiles and their corresponding wrapper, this would solve this
+      const handleCleanupWrapper = function (event) {
+        const tile = event.tile;
+        // Find all tile wrappers in the DOM
+        const wrappers = layer._level.el.querySelectorAll(".bib-tile-wrapper");
+
+        wrappers.forEach((wrapper) => {
+          // If the wrapper no longer has a tile inside it, so it would have only the 1 element of the flip side remove it
+          const containsTile = Array.from(wrapper.children).length === 1 ? false : true;
+
+          if (!containsTile) {
+            wrapper.remove();
+          }
+        });
+      }
+
+      layer.on("tileload", onTileLoad);
+      layer.on('tileloadstart', onTileLoadStart);
+
+      layer.on('tileabort', handleCleanupWrapper);
+      layer.on('tileunload', handleCleanupWrapper);
+
+      // when the page has fully loaded remove the animation listener
+      layer.on("load", function (event) {
+        layer.off("tileload", onTileLoad);
+        layer.off('tileloadstart', onTileLoadStart);
+        layer.off('tileabort', handleCleanupWrapper);
+        layer.off('tileerror', handleCleanupWrapper);
+      });
+
+
+      return () => {
+        layer.off("tileload");
+        layer.off('tileloadstart', onTileLoadStart);
+        layer.off('tileabort', handleCleanupWrapper);
+        layer.off('tileerror', handleCleanupWrapper);
+      };
+    });
+  }, [mapEvents]);
 
   if (!firstLoad) {
     const zoomMultiplier = getZoomMultiplier(mapEvents.getZoom());
