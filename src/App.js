c6c625dc3daf9e44be07f3bde7b5135e2005c6de
Szymon Maksymowicz <131286526+maxio02@users.noreply.github.com>
2025-03-06 18:06:18 +0100

    Fixed the load in animation not working after building to prod (#2)
    * feat: load-in animation working
    
    * feat: animation rotating with logo
    
    * fix: firefox compatibility
    
    * fix: retina display compatibility
    
    * fix: load in animation not playing
    
    * fix: animation would play multiple times during a session
    
    ---------
    
    Co-authored-by: Maurycy Jakiel <62938740+Jobieskii@users.noreply.github.com>
    Co-authored-by: Szymon Maksymowicz <smaksymowicz@griddynamics.com>

--- src/App.js
+++ src/App.js
@@ -46,6 +46,72 @@ function App({ center }) {
     }
   }
 
+  function handleCleanupWrapper(e) {
+    // Find all tile wrappers in the DOM
+    const layer = e.target;
+
+    const wrappers = layer._level.el.querySelectorAll(".bib-tile-wrapper");
+
+    wrappers.forEach((wrapper) => {
+      // If the wrapper no longer has a tile inside it, so it would have only the 1 element of the flip side remove it
+      const containsTile =
+        Array.from(wrapper.children).length === 1 ? false : true;
+
+      if (!containsTile) {
+        wrapper.remove();
+      }
+    });
+  }
+
+  function onTileLoad(e) {
+    const tile = e.tile;
+    const wrapper = tile.parentNode;
+
+    if (wrapper && wrapper.classList.contains("bib-tile-wrapper")) {
+      setTimeout(() => {
+        wrapper.classList.add("bib-tile-wrapper--animated");
+      }, Math.random() * 1000 + 100);
+    }
+  }
+
+  function onTileLoadStart(e) {
+    const tile = e.tile;
+
+    if (tile.hasAttribute("tile-wrapped")) return;
+    tile.setAttribute("tile-wrapped", "true");
+
+    tile.style.transform = "";
+    tile.classList.add("bib-leaflet-tile");
+
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("bib-tile-wrapper");
+    tile.parentNode.insertBefore(wrapper, tile);
+    wrapper.appendChild(tile);
+
+    const flippedTile = document.createElement("img");
+    flippedTile.classList.add("bib-leaflet-tile--flipped");
+    flippedTile.src = "loading-bib-tile.png";
+    wrapper.appendChild(flippedTile);
+
+    wrapper.style.translate = `${tile._leaflet_pos.x}px ${tile._leaflet_pos.y}px`;
+
+    tile.addEventListener("error", () => {
+      setTimeout(() => {
+        wrapper.classList.add("bib-tile-wrapper--animated");
+      }, Math.random() * 700 + 100);
+    });
+
+    wrapper.addEventListener("transitionend", () => {
+      tile.style.translate = wrapper.style.translate;
+      const parent = wrapper.parentNode;
+      if (parent) {
+        tile.classList.remove("bib-leaflet-tile");
+        tile.removeAttribute("tile-wrapped");
+        parent.insertBefore(tile, wrapper);
+        parent.removeChild(wrapper);
+      }
+    });
+  }
 
   const displayMap = useMemo(
     () => (
@@ -72,6 +138,24 @@ function App({ center }) {
           url="https://bib.bohenek.xyz/content/{z}/{x}/{y}.png?{buster}"
           buster={cacheBustIndex}
           detectRetina={true}
+          eventHandlers={
+            !sessionStorage.getItem("animPlayed")
+              ? {
+            tileloadstart: onTileLoadStart,
+            tileload: onTileLoad,
+            tileabort: handleCleanupWrapper,
+            tileunload: handleCleanupWrapper,
+            load: (event) => {
+              const layer = event.target;
+              layer.off("tileload");
+              layer.off("tileloadstart");
+              layer.off("tileabort");
+              layer.off("tileerror");
+                    sessionStorage.setItem("animPlayed", true);
+            },
+                }
+              : {}
+          }
         />
         <AttributionControl position="bottomleft" />
         <Logmap center={center}></Logmap>
@@ -185,114 +269,6 @@ function Logmap({ center }) {
       setLastState(0);
     };
     window.addEventListener('popstate', onPopstate);
-    
-    mapEvents.eachLayer((layer) => {
-
-      const onTileLoad = function (event) {
-        const tile = event.tile;
-
-        if (tile.parentNode && tile.parentNode.classList.contains("bib-tile-wrapper")) {
-          const wrapper = tile.parentNode;
-          // after a random interval add the aninmation
-          setTimeout(() => {
-            wrapper.classList.add("bib-tile-wrapper--animated");
-          }, Math.random() * 1000 + 100);
-        }
-      };
-
-
-      const onTileLoadStart = function (event) {
-        const tile = event.tile;
-        // remove the transform from leaflet and add class to flip the tiles
-
-        if (tile.hasAttribute("tile-wrapped")) {
-          return;
-        }
-        tile.setAttribute("tile-wrapped", "true");
-
-        tile.style.transform = "";
-        tile.classList.add("bib-leaflet-tile");
-
-        const wrapper = document.createElement("div");
-        wrapper.classList.add("bib-tile-wrapper");
-
-        tile.parentNode.insertBefore(wrapper, tile);
-        wrapper.appendChild(tile);
-
-        const flippedTile = document.createElement("img");
-        flippedTile.classList = tile.classList;
-        flippedTile.classList.add("bib-leaflet-tile--flipped");
-
-        // change this to get the infinity symbol
-
-        // flippedTile.src = 'android-chrome-512x512.png';
-        flippedTile.src = 'loading-bib-tile.png';
-        wrapper.appendChild(flippedTile);
-
-        // put back the translate as a indivual property instead of using transform
-        wrapper.style.translate = `${tile._leaflet_pos.x}px ${tile._leaflet_pos.y}px`;
-
-        // on the 404 trigger the flip animation 
-        tile.addEventListener('error', (event) => {
-          setTimeout(() => {
-            wrapper.classList.add("bib-tile-wrapper--animated");
-
-          }, Math.random() * 700 + 100);
-        });
-
-        // this brings everything back to the state that leaflet wants it in
-        wrapper.addEventListener('transitionend', (event) => {
-          tile.style.translate = wrapper.style.translate;
-
-          const parent = wrapper.parentNode;
-          if (parent) {
-            tile.classList.remove('bib-leaflet-tile');
-            tile.removeAttribute('tile-wrapped')
-            parent.insertBefore(tile, wrapper);
-            parent.removeChild(wrapper);
-          }
-        });
-      };
-
-
-      // this has to be done, because leaflet deletes the img from the wrapper on unload or abort, can happen on slow internet or on scrolling while its animating
-      // leaflet also deletes the tile/img first and then gives the event so, I cannot access the parent wrapper, so I basically have to search for dangling wrappers
-      // this function fucking sucks, its slow, and on every unload will do a shit ton of checks, but I give up
-
-      // the solution would be to just keep a map of all the tiles and their corresponding wrapper, this would solve this
-      const handleCleanupWrapper = function (event) {
-        // Find all tile wrappers in the DOM
-        const wrappers = layer._level.el.querySelectorAll(".bib-tile-wrapper");
-
-        wrappers.forEach((wrapper) => {
-          // If the wrapper no longer has a tile inside it, so it would have only the 1 element of the flip side remove it
-          const containsTile = Array.from(wrapper.children).length === 1 ? false : true;
-
-          if (!containsTile) {
-            wrapper.remove();
-          }
-        });
-      }
-
-      if (sessionStorage.getItem('anim')) {
-        return () => {}
-      }
-      sessionStorage.setItem('anim', true)
-
-      layer.on("tileload", onTileLoad);
-      layer.on('tileloadstart', onTileLoadStart);
-
-      layer.on('tileabort', handleCleanupWrapper);
-      layer.on('tileunload', handleCleanupWrapper);
-
-      // when the page has fully loaded remove the animation listener
-      layer.on("load", function (event) {
-        layer.off("tileload", onTileLoad);
-        layer.off('tileloadstart', onTileLoadStart);
-        layer.off('tileabort', handleCleanupWrapper);
-        layer.off('tileerror', handleCleanupWrapper);
-      });
-    });
 
     return () => {
       window.removeEventListener('popstate', onPopstate);
